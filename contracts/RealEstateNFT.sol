6729c3d0c241819e1b7fcbc1d7d1a51e0ed4afa4
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2025-06-12 09:43:01 -0700

    Update RealEstateNFT.sol

--- contracts/RealEstateNFT.sol
+++ contracts/RealEstateNFT.sol
@@ -1,1 +1,17 @@
-//
+
+
+
+contract RealEstateNFT is ERC721URIStorage, Ownable {
+    constructor() ERC721("RealEstateProperty", "REPROP") Ownable(msg.sender) {}
+    uint256 public nextTokenId = 1;
+
+    
+
+    function mintProperty(address to, string calldata tokenURI) external onlyOwner returns (uint256) {
+        uint256 tokenId = nextTokenId;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        nextTokenId += 1;
+        return tokenId;
+    }
+}
