c3f45911dba588c06e772f34b088bf37cbeeb779
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2025-06-21 12:54:12 -0700

    Update ZillowOracleConsumer.sol

--- contracts/ZillowOracleConsumer.sol
+++ contracts/ZillowOracleConsumer.sol
@@ -1,1 +1,22 @@
-//
+
+
+
+contract ZillowOracleConsumer is FunctionsClient, Ownable {
+    constructor(address router, address _porFeed) FunctionsClient(router) Ownable(msg.sender) {
+        porFeed = PoRFeed(_porFeed);
+    }
+    using FunctionsRequest for FunctionsRequest.Request;
+
+    string public sourceCode;
+    string public lastZillowURL;
+    string[] public args = new string[](1);
+
+    bytes32 public latestRequestId;
+    string public donId;
+    uint64 public subscriptionId;
+    uint32 public gasLimit = 300000;
+
+    PoRFeed public porFeed;
+    uint256 public tokenId; // Temp token ID to assign value to
+
+    
