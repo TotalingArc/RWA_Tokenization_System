6781e124a3903551317c35817b2396d1aea9294c
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2025-06-21 12:56:39 -0700

    Update PoRFeed.sol

--- contracts/PoRFeed.sol
+++ contracts/PoRFeed.sol
@@ -1,1 +1,33 @@
-//
+
+
+
+
+
+contract PoRFeed is Ownable {
+    constructor() Ownable(msg.sender) {}
+    mapping(uint256 => uint256) public values;
+    mapping(uint256 => bool) public confirmed;
+    address public oracle;
+
+    modifier onlyOracle() {
+        require(msg.sender == oracle, "Not oracle");
+        _;
+    }
+
+    function setOracle(address _oracle) external onlyOwner {
+        oracle = _oracle;
+    }
+
+    function confirmValue(uint256 tokenId, uint256 value) external onlyOracle {
+        values[tokenId] = value;
+        confirmed[tokenId] = true;
+    }
+
+    function isConfirmed(uint256 tokenId) external view returns (bool) {
+        return confirmed[tokenId];
+    }
+
+    function getValue(uint256 tokenId) external view returns (uint256) {
+        return values[tokenId];
+    }
+}
