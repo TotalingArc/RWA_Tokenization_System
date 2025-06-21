5f61f7e7dd0a90ea911165e17598450c1cc46e9d
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2025-06-21 12:55:21 -0700

    Update ZillowOracleConsumer.sol

--- contracts/ZillowOracleConsumer.sol
+++ contracts/ZillowOracleConsumer.sol
@@ -20,3 +20,29 @@ contract ZillowOracleConsumer is FunctionsClient, Ownable {
     uint256 public tokenId; // Temp token ID to assign value to
 
     
+ function updateConfig(string calldata _source, string calldata _donId, uint64 _subId, uint32 _gasLimit) external onlyOwner {
+        sourceCode = _source;
+        donId = _donId;
+        subscriptionId = _subId;
+        gasLimit = _gasLimit;
+    }
+
+    function sendZestimateRequest(string calldata zillowURL, uint256 _tokenId) external onlyOwner {
+        args[0] = zillowURL;
+        lastZillowURL = zillowURL;
+        tokenId = _tokenId;
+
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(sourceCode);
+        req.setArgs(args);
+        bytes memory encodedReq = req.encodeCBOR();
+
+        latestRequestId = _sendRequest(encodedReq, subscriptionId, gasLimit, bytes32(bytes(donId)));
+    }
+
+    function fulfillRequest(bytes32, bytes memory response, bytes memory err) internal override {
+        require(err.length == 0, "Oracle error");
+        uint256 zestimate = abi.decode(response, (uint256));
+        porFeed.confirmValue(tokenId, zestimate);
+    }
+}
