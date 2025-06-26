42f85b2f5df3fec1ebcfef667cdff7109be1da7d
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2025-06-25 19:56:06 -0700

    Update FunctionsRequest.sol

--- contracts/FunctionsRequest.sol
+++ contracts/FunctionsRequest.sol
@@ -1,51 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { IFunctionsRouter } from "./IFunctionsRouter.sol";
-import { IFunctionsClient } from "./IFunctionsClient.sol";
-import { FunctionsRequest } from "./FunctionsRequest.sol";
+library FunctionsRequest {
+    uint8 internal constant REQUEST_DATA_VERSION = 1;
 
-abstract contract FunctionsClient is IFunctionsClient {
-    using FunctionsRequest for FunctionsRequest.Request;
-
-    IFunctionsRouter internal immutable i_router;
-
-    event RequestSent(bytes32 indexed id);
-    event RequestFulfilled(bytes32 indexed id);
-
-    error OnlyRouterCanFulfill();
-
-    constructor(address router) {
-        i_router = IFunctionsRouter(router);
+    struct Request {
+        string source;
+        string[] args;
     }
 
-    function _sendRequest(
-        bytes memory data,
-        uint64 subscriptionId,
-        uint32 callbackGasLimit,
-        bytes32 donId
-    ) internal returns (bytes32) {
-        bytes32 requestId = i_router.sendRequest(
-            subscriptionId,
-            data,
-            FunctionsRequest.REQUEST_DATA_VERSION,
-            callbackGasLimit,
-            donId
-        );
-        emit RequestSent(requestId);
-        return requestId;
+    function initializeRequestForInlineJavaScript(Request memory req, string memory sourceCode) internal pure {
+        req.source = sourceCode;
     }
 
-    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal virtual;
+    function setArgs(Request memory req, string[] memory args) internal pure {
+        req.args = args;
+    }
 
-    function handleOracleFulfillment(
-        bytes32 requestId,
-        bytes memory response,
-        bytes memory err
-    ) external override {
-        if (msg.sender != address(i_router)) revert OnlyRouterCanFulfill();
-        fulfillRequest(requestId, response, err);
-        emit RequestFulfilled(requestId);
+    function encodeCBOR(Request memory req) internal pure returns (bytes memory) {
+        return abi.encode(req.source, req.args);
     }
 }
-
