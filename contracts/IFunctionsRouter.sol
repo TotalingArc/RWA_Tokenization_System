5cd70c5e1ce40b25fd50385a620516addb8d27e5
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2025-06-26 17:50:27 -0700

    Update IFunctionsRouter.sol

--- contracts/IFunctionsRouter.sol
+++ contracts/IFunctionsRouter.sol
@@ -1,1 +1,12 @@
-//
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IFunctionsRouter {
+    function sendRequest(
+        uint64 subscriptionId,
+        bytes calldata data,
+        uint8 dataVersion,
+        uint32 callbackGasLimit,
+        bytes32 donId
+    ) external returns (bytes32);
+}
