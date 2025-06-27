5387f00778a865e940029a7203bdb22f0298209c
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2025-06-26 17:49:39 -0700

    Update IFunctionsClient.sol

--- contracts/IFunctionsClient.sol
+++ contracts/IFunctionsClient.sol
@@ -1,1 +1,11 @@
-//
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+interface IFunctionsClient {
+    function handleOracleFulfillment(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) external;
+}
+
